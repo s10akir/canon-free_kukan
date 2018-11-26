d65418a2154207125aef573c2d93361e17ef2f85
Akira Shinohara <k017c1067@it-neec.jp>
2018-11-27 01:38:04 +0900

    [new] 簡易的な会員ログインを実装

--- app/controllers/terminal_controller.rb
+++ app/controllers/terminal_controller.rb
@@ -1,2 +1,15 @@
 class TerminalController < ApplicationController
+  def index
+
+  end
+
+  def login
+    begin
+      # idの連結
+      id = params['id_1'].to_i * 1_000_000_000_000 + params['id_2'].to_i * 100_000_000 + params['id_3'].to_i * 10_000 + params['id_4'].to_i
+      @user = User.find(id)
+    rescue
+      redirect_to action: 'index', flash: '会員証の読み込みに失敗しました。' unless @user
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/terminal', to: 'terminal#index'
+  post '/terminal/login', to: 'terminal#login'
 end
