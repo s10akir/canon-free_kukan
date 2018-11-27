40c817785ba72e4c2be76912ff65dc9ce271cc03
Akira Shinohara <k017c1067@it-neec.jp>
2018-11-27 14:55:51 +0900

    [change] default rootをterminal#indexへ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'terminal#index'
   get '/terminal', to: 'terminal#index'
   post '/terminal/login', to: 'terminal#login'
 end
