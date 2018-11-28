2dfa15a58e44f1a638aa2ebb54d5283cc2494a2b
Akira Shinohara <k017c1067@it-neec.jp>
2018-11-28 13:51:35 +0900

    [change] terminal/index

--- app/controllers/terminal_controller.rb
+++ app/controllers/terminal_controller.rb
@@ -9,7 +9,8 @@ class TerminalController < ApplicationController
       id = params['id_1'].to_i * 1_000_000_000_000 + params['id_2'].to_i * 100_000_000 + params['id_3'].to_i * 10_000 + params['id_4'].to_i
       @user = User.find(id)
     rescue
-      redirect_to action: 'index', flash: '会員証の読み込みに失敗しました。' unless @user
+      flash.now[:alert] = '会員カードの読み込みに失敗しました'
+      redirect_to action: 'index'
     end
   end
 end
